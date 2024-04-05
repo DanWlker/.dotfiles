538e3a64e76b10f2b441be75d4b1764672f2eced
DanWlker <danielhee2@gmail.com>
2024-04-05 23:43:14 +0800

    More stuff

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Macchiato"
+--config.color_scheme = "Catppuccin Frappe"
 --config.color_scheme = "Tokyo Night"
 config.font = wezterm.font("Fira Code")
 return config
