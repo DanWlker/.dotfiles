c5b2b484293cde1b5fceda757d27c53850e590ae
DanWlker <danielhee2@gmail.com>
2024-04-05 23:29:58 +0800

    Change theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,4 +1,6 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+config.color_scheme = "Catppuccin Macchiato"
+--config.color_scheme = "Tokyo Night"
 config.font = wezterm.font("Fira Code")
 return config
