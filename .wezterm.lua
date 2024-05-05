af179913d54429ca34adf04925717a70523244f4
DanWlker <danielhee2@gmail.com>
2024-05-05 13:14:34 +0800

    Add rose pine theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,5 +3,6 @@ local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Macchiato"
 --config.color_scheme = "Catppuccin Frappe"
 --config.color_scheme = "Tokyo Night"
+-- config.color_scheme = 'rose-pine'
 config.font = wezterm.font("MesloLGS NF")
 return config
