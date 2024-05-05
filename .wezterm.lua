21e3a04bcaa8ec06358ceb3824613d7ab1b87865
DanWlker <danielhee2@gmail.com>
2024-05-05 20:47:41 +0800

    Add dracula theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Macchiato"
+-- config.color_scheme = "Dracula (Official)"
 --config.color_scheme = "Catppuccin Frappe"
 --config.color_scheme = "Tokyo Night"
 -- config.color_scheme = 'rose-pine'
