7a1f31753fb1d947c6c292852dda77f1aecd448b
DanWlker <danielhee2@gmail.com>
2025-05-16 23:11:53 +0800

    update installer script

--- stow_ignored/scripts/install.sh
+++ stow_ignored/scripts/install.sh
@@ -1,8 +1,21 @@
 # Ask for admin priviledge upfront
 sudo -v
 
+# Install zsh if on linux
+if [ "$(uname)" != "Darwin" ]; then
+	sudo apt-get update
+	sudo apt install curl zsh
+fi
+
 # Download brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [ -d "/opt/homebrew" ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [ -d "~/.linuxbrew" ]; then
+  eval "$(~/.linuxbrew/bin/brew shellenv)"
+elif [ -d "/home/linuxbrew" ]; then
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
 
 # Download dependencies
 xargs brew install < $HOME/.dotfiles/stow_ignored/my_brews
@@ -10,11 +23,6 @@ xargs brew install < $HOME/.dotfiles/stow_ignored/my_brews
 # Verify everything is installed
 chmod +x $HOME/.dotfiles/stow_ignored/scripts/verify.sh
 $HOME/.dotfiles/stow_ignored/scripts/verify.sh
-
-# Install zsh if on linux
-if [ "$(uname)" != "Darwin" ]; then
-	sudo apt install zsh
-fi
 
 # Link stuff in the correct location
 cd ~/.dotfiles/ && stow .
