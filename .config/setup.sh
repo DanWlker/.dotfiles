b9b026b244c4c30b11b7246dd7db7a89d0658a2d
DanWlker <danielhee2@gmail.com>
2025-06-07 11:01:05 +0800

    kanagawa-paper theme

--- .config/setup.sh
+++ .config/setup.sh
@@ -1,11 +1,9 @@
 # fzf
 if which fzf &>/dev/null; then
 	export FZF_DEFAULT_OPTS=" \
-		--color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
-		--color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
-		--color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
-		--color=selected-bg:#45475A \
-		--color=border:#313244,label:#CDD6F4"
+		--color=bg:-1,bg+:#2A2A37,fg:-1,fg+:#DCD7BA,hl:#938AA9,hl+:#c4746e \
+		--color=header:#b6927b,info:#658594,pointer:#7AA89F \
+		--color=marker:#7AA89F,prompt:#c4746e,spinner:#8ea49e"
 	source <(fzf --zsh)
 fi
 
