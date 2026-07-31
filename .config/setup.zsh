b75d6c29850fdc88e187b236f7e6345f9f658a7b
Hee ZiXi <zixi.hee@paynet.my>
2026-07-31 17:22:17 +0800

    remove fzf-tab and use brew

--- .config/setup.zsh
+++ .config/setup.zsh
@@ -194,22 +194,24 @@ if which terraform &>/dev/null; then
 fi
 
 # fzf-tab
-# disable sort when completing `git checkout`
-zstyle ':completion:*:git-checkout:*' sort false
-# set descriptions format to enable group support
-# NOTE: don't use escape sequences (like '%F{red}%d%f') here, fzf-tab will ignore them
-zstyle ':completion:*:descriptions' format '[%d]'
-# set list-colors to enable filename colorizing
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-# force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
-zstyle ':completion:*' menu no
-# preview directory's content with eza when completing cd
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 -a --color=always $realpath'
-# NOTE: This may lead to unexpected behavior since some flags break this plugin. See https://github.com/Aloxaf/fzf-tab/issues/455
-zstyle ':fzf-tab:*' use-fzf-default-opts yes
-# switch group using tab
-zstyle ':fzf-tab:*' switch-group 'btab' 'tab'
-source "$HOME/fzf-tab/fzf-tab.plugin.zsh"
+if [[ -f "$(brew --prefix)/opt/fzf-tab/share/fzf-tab/fzf-tab.zsh" ]]; then
+	# disable sort when completing `git checkout`
+	zstyle ':completion:*:git-checkout:*' sort false
+	# set descriptions format to enable group support
+	# NOTE: don't use escape sequences (like '%F{red}%d%f') here, fzf-tab will ignore them
+	zstyle ':completion:*:descriptions' format '[%d]'
+	# set list-colors to enable filename colorizing
+	zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+	# force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+	zstyle ':completion:*' menu no
+	# preview directory's content with eza when completing cd
+	zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 -a --color=always $realpath'
+	# NOTE: This may lead to unexpected behavior since some flags break this plugin. See https://github.com/Aloxaf/fzf-tab/issues/455
+	zstyle ':fzf-tab:*' use-fzf-default-opts yes
+	# switch group using tab
+	zstyle ':fzf-tab:*' switch-group 'btab' 'tab'
+	source "$(brew --prefix)/opt/fzf-tab/share/fzf-tab/fzf-tab.zsh"
+fi
 
 # zsh-system-clipboard
 if [[ -f $(brew --prefix 2>/dev/null)/share/zsh-system-clipboard/zsh-system-clipboard.zsh ]]; then
