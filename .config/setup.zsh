206380f526cc11c1bce5d23864605aa9fa411fdd
DanWlker <danielhee2@gmail.com>
2026-08-05 12:52:22 +0800

    remove previews for fzf tab cause they buggy af

--- .config/setup.zsh
+++ .config/setup.zsh
@@ -229,8 +229,6 @@ if [[ -f "$HOMEBREW_PREFIX/opt/fzf-tab/share/fzf-tab/fzf-tab.zsh" ]]; then
 	zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 	# force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
 	zstyle ':completion:*' menu no
-	# preview directory's content with eza when completing cd
-	zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 -a --color=always $realpath'
 	# NOTE: This may lead to unexpected behavior since some flags break this plugin. See https://github.com/Aloxaf/fzf-tab/issues/455
 	zstyle ':fzf-tab:*' use-fzf-default-opts yes
 	# switch group using < >
